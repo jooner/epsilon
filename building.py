29d29b44c0920f3469a6e018bf5427381846c26d
Joon Yang <joonhyukyang@college.harvard.edu>
2017-07-26 17:19:14 +0900

    building with current floor value

--- building.py
+++ building.py
@@ -1,13 +1,19 @@
 from globals import *
+from elevator import Elevator
 
 class Building(object):
     def __init__(self):
         self.floors = []
-        for _ in xrange(NUM_FLOORS):
-            self.floors.append(Floor())
+        for i in xrange(NUM_FLOORS):
+            floor = Floor(i)
+            self.floors.append(floor)
+        self.elevators = []
+        for _ in xrange(NUM_ELEVATORS):
+            self.elevators.append(Elevator())
 
 class Floor(object):
-    def __init__(self):
+    def __init__(self, floor):
+        self.value = floor
         self.passenger_list = [] # list of passenger objects
         self.is_elevator = False # boolean
         self.call = [0,0]
