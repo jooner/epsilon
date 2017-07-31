fixed LQF average scoring

baseline/LQF.py
@@ -9,7 +9,6 @@ where multiple elevator cars arrive at the same floor simultaneously, often
 occurs in practice.
 
 """
-from __future__ import absolute_import
 
 from environment import Environment
 from building import Building
@@ -17,7 +16,7 @@ from globals import *
 
 import numpy as np
 
-NUM_EPOCHS = 1
+NUM_EPOCHS = 100
 
 
 def lqf_controller(env):
@@ -30,7 +29,7 @@ def lqf_controller(env):
             priority_flr = priority_q[min(priority_q.keys())]
             action[i] = np.sign(priority_flr - elevator.curr_floor)
         else: # elevator is moving
-            ## NOTE: Doesn't consider when to stop for loading -- only considers when to 
+            ## NOTE: Doesn't consider when to stop for loading -- only considers when to
             ## stop for unloading.
             is_upcall = env.building.floors[elevator.curr_floor].call[0]
             is_downcall = env.building.floors[elevator.curr_floor].call[1]
@@ -45,7 +44,7 @@ def game_over(env):
     return (env.total_pop >= MAX_POPULATION)
 
 def lqf_run(epoch=1):
-    #scores = []
+    result = 0
     for i in range(epoch):
         lqf_building = Building()
         lqf_env = Environment(lqf_building)
@@ -61,9 +60,8 @@ def lqf_run(epoch=1):
         #scores.append(lqf_env.get_reward())
         lqf_env.update_global_time_list()
         avg_time = sum(lqf_env.global_time_list) / float(lqf_env.total_pop)
-    return avg_time
-    # average performance over epochs
-    # return sum(scores) / len(scores)
+        result += avg_time
+    return result / float(epoch)
 
 def lqf_main():
     average_score = lqf_run(epoch=NUM_EPOCHS)