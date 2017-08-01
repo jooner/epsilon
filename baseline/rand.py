d6b9223769af3b423f07330bc7fcf484de04c532
Joon Sik Kim <davidkim21c@gmail.com>
2017-08-01 11:39:47 +0900

    Random basline works bettern than any other. Working on the test scenario of the baselines.

---
+++ baseline/rand.py
@@ -0,0 +1,50 @@
+"""
+Implementation of Baseline Model Random:
+
+Randomly assign the elevators to random floors.
+
+"""
+import sys 
+
+if "../" not in sys.path:
+  sys.path.append("../")
+
+from environment import *
+from building import *
+from globals import *
+
+import numpy as np
+
+NUM_EPOCHS = 100
+
+
+def random_controller(env):
+    action = [np.random.randint(3) - 1 for i in range(len(env.building.elevators))]
+    return action
+
+def game_over(env):
+    return (env.total_pop >= MAX_POPULATION)
+
+def random_run(epoch=1):
+    result = 0
+    for i in range(epoch):
+        random_building = Building()
+        random_env = Environment(random_building)
+        #print "---------------------%d"%i
+
+        while not game_over(random_env):
+            random_env.populate() # populate the building
+            random_env.tic() # t += 1
+            action = random_controller(random_env)
+            s, r, _ = random_env.step(action)
+        random_env.update_global_time_list()
+        avg_time = sum(random_env.global_time_list) / float(random_env.total_pop)
+        result += avg_time
+    return result / float(epoch)
+
+def random_main():
+    average_score = random_run(epoch=NUM_EPOCHS)
+    print("[Random Baseline]\tAverage Score: {} over {} Epochs".format(average_score, NUM_EPOCHS))
+
+if __name__ == "__main__":
+    random_main()
