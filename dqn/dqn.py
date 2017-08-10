cbd0f8ac8c4d96fa91a2e187fdc3d26e87591818
Joon Yang <joonhyukyang@college.harvard.edu>
2017-08-10 17:52:26 +0900

    playing with params

--- dqn/dqn.py
+++ dqn/dqn.py
@@ -52,16 +52,15 @@ class Estimator():
         self.actions_pl = tf.placeholder(shape=[None], dtype=tf.int32, name="actions")
         X = tf.expand_dims(tf.to_float(self.inputs),-1)
         conv1 = tf.contrib.layers.conv2d(
-            X, num_outputs=32, kernel_size=3, stride=2, activation_fn=tf.nn.relu)
+            X, num_outputs=64, kernel_size=3, stride=2, activation_fn=tf.nn.relu)
         conv2 = tf.contrib.layers.conv2d(
-            conv1, 16, 2, 1, activation_fn=tf.nn.relu)
+            conv1, 32, 2, 2, activation_fn=tf.nn.relu)
         conv3 = tf.contrib.layers.conv2d(
             conv2, 8, 2, 1, activation_fn=tf.nn.relu)
         flattened = tf.contrib.layers.flatten(conv3)
         # Fully connected layers with RELU
         fc1 = tf.contrib.layers.fully_connected(flattened, 128)
-        fc2 = tf.contrib.layers.fully_connected(flattened, 64)
-        self.predictions = tf.contrib.layers.fully_connected(fc2, self.a_dim, activation_fn=None)
+        self.predictions = tf.contrib.layers.fully_connected(fc1, self.a_dim, activation_fn=None)
         self.action_predictions = tf.gather(tf.reshape(self.predictions, [-1]), self.actions_pl)
 
 
